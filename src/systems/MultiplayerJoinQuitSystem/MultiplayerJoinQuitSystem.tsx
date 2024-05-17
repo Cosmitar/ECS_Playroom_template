5899468a6a34666cd9445dd4e2bff583ddbfc067
Cosmitar <mhherrera31@gmail.com>
2024-05-17 07:53:14 +0200

    dev: listen join/quit on launch event + trust in usePlayersList

--- src/systems/MultiplayerJoinQuitSystem/MultiplayerJoinQuitSystem.tsx
+++ src/systems/MultiplayerJoinQuitSystem/MultiplayerJoinQuitSystem.tsx
@@ -1,26 +1,30 @@
 /**
  * Handles player join and quit. Keeps network players in sync with local entities.
  */
-import { PlayerState, onPlayerJoin } from 'playroomkit'
+import { PlayerState, onPlayerJoin, usePlayersList } from 'playroomkit'
 import { useEffect } from 'react'
-import useConsistentPlayerList from '../../utils/useConsistentPlayerList'
 import { PlayersQuery } from '../../state/queries'
 import { createPlayer } from '../../entities/Players/Players'
 import { ColorRepresentation } from 'three'
 import { ECS } from '../../state/ECS'
+import { ON_LAUNCH } from '../InsertCoinSystem/InsertCoinSystem'
 
 export default function MultiplayerJoinQuitSystem() {
-  useEffect(() => {
-    onPlayerJoin(player => {
-      // on join we do nothing, just let usePlayerList to update for entities maintenance
+  useEffect(
+    () =>
+      ON_LAUNCH.subscribe(() => {
+        onPlayerJoin(player => {
+          // on join we do nothing, just let usePlayerList to update for entities maintenance
 
-      player.onQuit(() => {
-        removePlayer(player)
-      })
-    })
-  }, [])
+          player.onQuit(() => {
+            removePlayer(player)
+          })
+        })
+      }),
+    []
+  )
 
-  const playerList = useConsistentPlayerList()
+  const playerList = usePlayersList(false)
 
   useEffect(() => {
     setupPlayers(playerList)
@@ -35,7 +39,8 @@ const setupPlayers = (players: PlayerState[]) => {
 
   players.map(p => {
     if (!ids.includes(p.id)) {
-      createPlayer(p, p.getProfile().color.hexString as ColorRepresentation)
+      const color = p.getProfile().color ? p.getProfile().color.hexString : '#999'
+      createPlayer(p, color as ColorRepresentation)
     }
   })
 }
