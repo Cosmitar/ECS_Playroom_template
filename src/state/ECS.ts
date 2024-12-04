d0ee72c1076f237934371a71a03cae7736fa7610
Cosmitar <mhherrera31@gmail.com>
2024-12-04 19:19:52 +0100

    chore: remove Miniplex implementations

--- src/state/ECS.ts
+++
@@ -1,7 +0,0 @@
-import { World } from 'miniplex'
-import { createReactAPI } from 'miniplex-react'
-import { Entity } from './types'
-
-const world = new World<Entity>()
-
-export const ECS = createReactAPI(world)
